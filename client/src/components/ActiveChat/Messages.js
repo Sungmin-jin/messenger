42c856908c9fc07d74c5bc39e5e8b313bc18e1b0
Sungmin-jin <jasonjin1998@gmail.com>
2021-07-30 17:08:29 -0700

    fix - change postMessage function to asyc function

--- client/src/components/ActiveChat/Messages.js
+++ client/src/components/ActiveChat/Messages.js
@@ -5,7 +5,7 @@ import moment from "moment";
 
 const Messages = (props) => {
   const { messages, otherUser, userId } = props;
-
+  console.log("messages", messages);
   return (
     <Box>
       {messages.map((message) => {
@@ -14,7 +14,12 @@ const Messages = (props) => {
         return message.senderId === userId ? (
           <SenderBubble key={message.id} text={message.text} time={time} />
         ) : (
-          <OtherUserBubble key={message.id} text={message.text} time={time} otherUser={otherUser} />
+          <OtherUserBubble
+            key={message.id}
+            text={message.text}
+            time={time}
+            otherUser={otherUser}
+          />
         );
       })}
     </Box>
