a3b305ede65c1d4764e31bafc4c3e20c560fa567
Sungmin-jin <jasonjin1998@gmail.com>
2021-08-06 14:26:13 -0700

    fix bug - app crashed when register

--- client/src/store/utils/thunkCreators.js
+++ client/src/store/utils/thunkCreators.js
@@ -38,7 +38,7 @@ export const register = (credentials) => async (dispatch) => {
     const { data } = await axios.post("/auth/register", credentials);
     await localStorage.setItem("messenger-token", data.token);
     dispatch(gotUser(data));
-    socket.emit("go-online");
+    socket.socket.emit("go-online");
   } catch (error) {
     console.error(error);
     dispatch(gotUser({ error: error.response.data.error || "Server Error" }));
@@ -64,7 +64,7 @@ export const logout = (id) => async (dispatch) => {
     await axios.delete("/auth/logout");
     await localStorage.removeItem("messenger-token");
     dispatch(gotUser({}));
-    socket.emit("logout");
+    socket.socket.emit("logout");
   } catch (error) {
     console.error(error);
   }
